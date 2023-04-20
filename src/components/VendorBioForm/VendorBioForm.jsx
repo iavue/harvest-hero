449087d271d87fe9c791939ce7e7e4abe4927987
Ia Vue <iavue00@gmail.com>
2023-04-20 18:46:05 -0500

    Fixing the VendorBioForm component to render as expected when clicking edit and save changes buttons.

--- src/components/VendorBioForm/VendorBioForm.jsx
+++ src/components/VendorBioForm/VendorBioForm.jsx
@@ -12,6 +12,14 @@ function VendorBioForm() {
         stall_num: ''
     });
 
+    const [editing, setEditing] = useState(true); // State to track if form is being edited or not
+    const [bioEditing, setBioEditing] = useState(true);
+    const [bioIdToEdit, setBioIdToEdit] = useState('')
+    const [newVendorName, setNewVendorName] = useState('');
+    const [newBioDescription, setNewBioDescription] = useState('');
+    const [newLocation, setNewLocation] = useState('');
+    const [newPmtMethods, setNewPmtMethods] = useState('');
+    const [newStallNum, setNewStallNum] = useState('');
     const dispatch = useDispatch();
 
     const submitHandler = (event) => {
@@ -20,6 +28,7 @@ function VendorBioForm() {
         console.log('What is inside my newBio for VendorBioForm:', newBio);
         if (newBio.vendor_name && newBio.bio_description && newBio.location && newBio.pmt_methods && newBio.stall_num) {
             dispatch({ type: 'ADD_BIO', payload: newBio });
+            setEditing(false); // Set editing state to false after submitting the form
         }
     }
 
@@ -50,25 +59,56 @@ function VendorBioForm() {
 
 
     return (
-        <form onSubmit={submitHandler}>
+        <>
+            {editing ? (
+                <form onSubmit={submitHandler}>
 
-            <div className="container">
+                    <div className="container">
 
-                {/* Image upload might become a stretch. 
-                    Need to figure out how to get images to stay on the DOM using multer! */}
-                {/* <p>Select a profile image to upload.</p>
-                <input type="file"></input> */}
+                        {/* Image upload might become a stretch. 
+                        Need to figure out how to get images to stay on the DOM using multer! */}
+                        {/* <p>Select a profile image to upload.</p>
+                            <input type="file"></input> */}
 
-                <input id='1' placeholder="name" value={newBio.vendor_name} onChange={setBio}></input>
-                <input id='2' placeholder="bio" value={newBio.bio_description} onChange={setBio}></input>
-                <input id='3' placeholder="location" value={newBio.location} onChange={setBio}></input>
-                <input id='4' placeholder="payment methods accepted" value={newBio.pmt_methods} onChange={setBio}></input>
-                <input id='5' placeholder="stall number" value={newBio.stall_num} onChange={setBio}></input>
-                <button type="submit">✔️</button>
+                        <input id='1' placeholder="name" value={newBio.vendor_name} onChange={setBio}></input>
+                        <input id='2' placeholder="bio" value={newBio.bio_description} onChange={setBio}></input>
+                        <input id='3' placeholder="location" value={newBio.location} onChange={setBio}></input>
+                        <input id='4' placeholder="payment methods accepted" value={newBio.pmt_methods} onChange={setBio}></input>
+                        <input id='5' placeholder="stall number" value={newBio.stall_num} onChange={setBio}></input>
+                        <button type="submit">✔️</button>
+                        {/* onsubmit, the onSubmit handler will set editing to false.
+                        once editing = false, then the below div will render and we
+                        will not see the form anymore. */}
 
-            </div>
+                    </div>
 
-        </form>
+                </form>
+            ) : (bioEditing ? (
+                <div>
+                    <p>Name: {newBio.vendor_name}</p>
+                    <p>Stall #: {newBio.stall_num}</p>
+                    <p>Payment methods accepted: {newBio.pmt_methods}</p>
+                    <p>Bio: {newBio.bio_description}</p>
+                    <p>Location: {newBio.location}</p>
+                    <button onClick={() => setBioEditing(false)}>Edit</button>
+                </div>
+            ) : (
+                <div>
+                    <input value={newVendorName} onChange={(event) => setNewVendorName(event.target.value)}></input>
+                    <input value={newBioDescription} onChange={(event) => setNewVendorName(event.target.value)}></input>
+                    <input value={newLocation} onChange={(event) => setNewLocation(event.target.value)}></input>
+                    <input value={newPmtMethods} onChange={(event) => setNewPmtMethods(event.target.value)}></input>
+                    <input value={newStallNum} onChange={(event) => setNewStallNum(event.target.value)}></input>
+                    {/* TO DO: fix the way how this is supposed to be showing!!!!!
+                    After clicking save changes the update should be sent to the server => database to update the what's
+                    in the database. 
+                    Then the above DIV should display instead with the updated info from the DB!!! */}
+                    {/* <button onClick={() => updateItem(newBio.id)}>Save Changes</button> */}
+                    {/* <button onClick={() => setBioEditing(false)}>Save Changes</button> */}
+                </div>
+            )
+            )}
+        </>
     )
 }
 
